cb1462cd8ebc49623f83a4a99d18f5e2520a6626
Kunal Kumar <kunalkumar7091679623@gmail.com>
2023-09-11 20:01:11 +0530

    starting with login_page

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import "./App.css";
+
+function App() {
+ 
+
+  return (
+    <div className="App">
+     starting
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import NevBar from './Navbar'
+// import Signup from './signUp'
+
+
+const Home = (props) => {
+  return (
+    <div>
+      <NevBar/>
+      <div className='container' style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+       <h1>{ props.name ? `welcome - ${props.name}` : " Login Please" }</h1>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Home
